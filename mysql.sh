ba84befa99391151528a540f7c661adcd6c4fa2a
Nagendra-123 <nagendraputti47@gmail.com>
2025-07-24 21:27:23 +0530

    Expense-shell

--- mysql.sh
+++ mysql.sh
@@ -26,10 +26,10 @@ VALIDATE()
 {
   if [ $1 -ne 0 ]
   then 
-      echo -e  "$2 is $R failed.... $N" | tee -a $LOG_FILE
+      echo -e "$2 is $R failed.... $N" | tee -a $LOG_FILE
       exit 1
   else 
-     echo -e "$2 is $Gsuccess$N" | tee -a $LOG_FILE
+      echo -e "$2 is $G success $N" | tee -a $LOG_FILE
   fi      
 }
 
