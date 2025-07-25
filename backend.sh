0f70044e21cff259b54b0634167e5f8c2c6290e4
Nagendra-123 <nagendraputti47@gmail.com>
2025-07-25 12:30:20 +0530

    Expense-shell

--- backend.sh
+++ backend.sh
@@ -63,5 +63,25 @@ VALIDATE $? "Downloadig backend application code"
 
 cd /app
 rm -rf /app/* #remove the existing code
-unzip /tmp/backend.zip
+unzip /tmp/backend.zip &>>$LOG_FILE
 VALIDATE $? "Extracting backend application code"
+
+npm install &>>$LOG_FILE
+cp /home/ec2-user/Expense-shell/backend.service /etc/systemd/system/backend.service
+
+#load the data before starting backend
+
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "Installing MySQL client"
+
+mysql -h backend-server.daws81.space -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
+VALIDATE $? "Schema loading"
+
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Daemon reload"
+
+systemctl enable backend &>>$LOG_FILE
+VALIDATE $? "Enable backend"
+
+systemctl restart backend &>>$LOG_FILE
+VALIDATE $? "Restart backend"
